0d2fd75b3b9d5bc42fcdfb704199f4e88c96816f
Raulshadow <50181593+Raulshadow@users.noreply.github.com>
2025-06-30 16:09:49 -0300

    Atualização das rotas e das views
    Estava colocando de uma forma um pouco mais genérica, contudo, lendo a documentação descobri que posso fazer dessa forma, facilitando a construção das rotas.
    
    Alterei as views para se utilizar o ModelViewSet visto que apenas quero trabalhar com CRUD geral, abrindo a api para executar as 4 para cada modelo.

--- symbaproject/api/urls.py
+++ symbaproject/api/urls.py
@@ -1,7 +1,22 @@
-from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import *
+
+router = DefaultRouter()
+router.register(r'usuarios', UsuarioViewSet)
+router.register(r'jogadores', JogadorViewSet)
+router.register(r'personagens', PersonagemViewSet)
+router.register(r'habilidades', HabilidadeViewSet)
+router.register(r'descricoes-habilidade', DescricaoHabilidadeViewSet)
+router.register(r'qualidades', QualidadeViewSet)
+router.register(r'equipamentos', EquipamentoViewSet)
+router.register(r'elixires', ElixirViewSet)
+router.register(r'armas', ArmaViewSet)
+router.register(r'armaduras', ArmaduraViewSet)
+router.register(r'artefatos', ArtefatoViewSet)
+router.register(r'poderes', PoderViewSet)
+router.register(r'aprendizados', AprendeViewSet)
 
 urlpatterns = [
-    path('jogadores/', views.JogadorListCreateView.as_view(), name='jogador-list-create-view'),
-    path('usuarios/', views.UsuarioListCreateView.as_view(), name='usuario-list-create-view'),
+    path('', include(router.urls)),
 ]
--- symbaproject/api/views.py
+++ symbaproject/api/views.py
@@ -1,14 +1,56 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 from .models import Usuario, Jogador, Habilidade, DescricaoHabilidade, Qualidade, Equipamento, Elixir, Arma, Armadura, Artefato, Poder, Personagem, Aprende
 from .serializers import UsuarioSerializer, JogadorSerializer, HabilidadeSerializer, DescricaoHabilidadeSerializer, QualidadeSerializer, EquipamentoSerializer, ElixirSerializer, ArmaSerializer, ArmaduraSerializer, ArtefatoSerializer, PoderSerializer, PersonagemSerializer, AprendeSerializer
 
-class JogadorListCreateView(generics.ListCreateAPIView):
-    """View para listar e criar Jogadores."""
-    queryset = Jogador.objects.all()
-    serializer_class = JogadorSerializer
-    
-class UsuarioListCreateView(generics.ListCreateAPIView):
-    """View para listar e criar Usuários."""
+class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+class JogadorViewSet(ModelViewSet):
+    queryset = Jogador.objects.all()
+    serializer_class = JogadorSerializer
+
+class PersonagemViewSet(ModelViewSet):
+    queryset = Personagem.objects.all()
+    serializer_class = PersonagemSerializer
+    
+class HabilidadeViewSet(ModelViewSet):
+    queryset = Habilidade.objects.all()
+    serializer_class = HabilidadeSerializer
+
+class DescricaoHabilidadeViewSet(ModelViewSet):
+    queryset = DescricaoHabilidade.objects.all()
+    serializer_class = DescricaoHabilidadeSerializer
+
+class QualidadeViewSet(ModelViewSet):
+    queryset = Qualidade.objects.all()
+    serializer_class = QualidadeSerializer
+
+class EquipamentoViewSet(ModelViewSet):
+    queryset = Equipamento.objects.all()
+    serializer_class = EquipamentoSerializer
+
+class ElixirViewSet(ModelViewSet):
+    queryset = Elixir.objects.all()
+    serializer_class = ElixirSerializer
+
+class ArmaViewSet(ModelViewSet):
+    queryset = Arma.objects.all()
+    serializer_class = ArmaSerializer
+
+class ArmaduraViewSet(ModelViewSet):
+    queryset = Armadura.objects.all()
+    serializer_class = ArmaduraSerializer
+
+class ArtefatoViewSet(ModelViewSet):
+    queryset = Artefato.objects.all()
+    serializer_class = ArtefatoSerializer
+    
+class PoderViewSet(ModelViewSet):
+    queryset = Poder.objects.all()
+    serializer_class = PoderSerializer
+    
+class AprendeViewSet(ModelViewSet):
+    queryset = Aprende.objects.all()
+    serializer_class = AprendeSerializer
